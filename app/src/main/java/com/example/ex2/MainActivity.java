5c51f2b2da1c6d0d4d5a7360eef7d7373c17b5cb
אביב <aviv.zecharia@mail.huji.ac.il>
2017-03-21 14:08:38 +0200

    2nd commit, still testing

---
+++ app/src/main/java/com/example/ex2/MainActivity.java
@@ -0,0 +1,135 @@
+package com.example.ex2;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final List<String> todo_list = new ArrayList<>();
+    private myAdapter adapter;
+    private ListView listView;
+    private EditText editText;
+    private int i = 1;
+
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+        //
+
+
+
+//      recycle view list ------------------------------------------------------------
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+//        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        mAdapter = new MyListAdapter(todo_list);
+        mRecyclerView.setAdapter(mAdapter);
+//      recycle view list ------------------------------------------------------------
+
+
+        this.adapter = new myAdapter(this, android.R.layout.simple_list_item_1, todo_list);
+        this.listView = (ListView) findViewById(R.id.list_view);
+        listView.setAdapter(adapter);
+
+//        editText = (EditText) findViewById(R.id.editText);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//              String message = String.valueOf(editText.getText());
+                String message = "Item "+(i++);
+//                editText.setText("");
+                todo_list.add(message);
+                adapter.notifyDataSetChanged();
+//                mAdapter.notifyDataSetChanged();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    class myAdapter<T> extends ArrayAdapter<T> {
+
+        float textSize = 0;
+        int unit = 0;
+
+        myAdapter(Context context, int resource, List<T> objects) {
+            super(context, resource, objects);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view = super.getView(position, convertView, parent);
+            if (position % 2 == 1) {
+                view.setBackgroundColor(Color.argb(150, 82, 116, 250));
+            } else {
+                view.setBackgroundColor(Color.argb(150, 250, 67, 67));
+            }
+
+            return view;
+        }
+    }
+}
+
+
+
